363b346263c7ce24d684e1abc5ee63474a1c9883
Notebook HP <Notebook HP@DESKTOP-5J4O1BA>
2023-09-16 16:54:11 -0500

    Agrege una linea

--- src/modelos/Main.java
+++ src/modelos/Main.java
@@ -12,6 +12,8 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        
+        System.out.println("HOLA q");
     }
     
 }
